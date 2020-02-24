e4a4e310af50ef81a37cd807ebc1a6e45e936ecc
tomarkhushboo <tomarkhushboo@users.noreply.github.com>
2020-02-24 17:10:09 +0530

    Update StringReverse.java

--- BasicOfJava/src/interviewProg/StringReverse.java
+++ BasicOfJava/src/interviewProg/StringReverse.java
@@ -1,10 +1,23 @@
 package interviewProg;
 
+import java.util.Scanner;
+
 public class StringReverse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Reverse the string
+
+		Scanner s = new Scanner(System.in);
+		char[] ar = s.nextLine().toCharArray();
+		System.out.println("string to be reversed" +s);
+		
+		
+		for(int i =ar.length-1;i>=0;i--) {
+			System.out.println(ar[i]);
+		}
+		
 
 	}
-
 }
+
+
